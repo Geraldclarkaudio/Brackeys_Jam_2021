45e9fe12033bc3f71b68ce99f6d42e702cf6801b
geraldclarkaudio <geraldclarkaudio@gmail.com>
2021-08-22 12:04:46 -0500

    Player Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Controller Settings")]
+    [SerializeField]
+    private float _speed = 5.0f;
+    [SerializeField]
+    private float _jumpHeight = 8.0f;
+    [SerializeField]
+    private float _gravity = 15.0f;
+    private CharacterController _controller;
+    private float yVel;
+    private Vector3 velocity;
+    private Vector3 direction;
+
+    [Header("Camera Settings")]
+    [SerializeField]
+    private float _cameraSensitivity = 2.0f;
+    private Camera mainCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = Camera.main;
+
+        if (mainCam == null)
+        {
+            Debug.LogError("main cam is null");
+        }
+
+        _controller = GetComponent<CharacterController>();
+
+        if (_controller == null)
+        {
+            Debug.LogError("Character Controller is Null");
+        }
+
+        //lock cursor and hide it
+        //escape key to unlock and reshow
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+        CameraRotation();
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    void CalculateMovement()
+    {
+        if (_controller.isGrounded == true)
+        {
+            Debug.Log("Youre Grounded");
+
+
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+             direction = new Vector3(horizontal, 0, vertical);
+             velocity = direction * _speed;
+
+            //CONVERT TO WORLD SPACE
+            velocity = transform.TransformDirection(velocity);
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                velocity.y += _jumpHeight;
+            }
+        }
+
+        velocity.y -= _gravity * Time.deltaTime;
+
+        _controller.Move(velocity * Time.deltaTime);
+    }
+
+    void CameraRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+
+        Vector3 currentRotation = transform.localEulerAngles;
+        currentRotation.y += mouseX * _cameraSensitivity;
+        transform.localRotation = Quaternion.AngleAxis(currentRotation.y, Vector3.up);
+
+
+
+        //apply mousey to camera roation x (look Up and Down)
+        //clamp between 0 and 15
+        Vector3 currentCamRot = mainCam.gameObject.transform.localEulerAngles;
+        currentCamRot.x -= mouseY * _cameraSensitivity;
+        currentCamRot.x = Mathf.Clamp(currentCamRot.x, 0, 26);
+        mainCam.gameObject.transform.localRotation = Quaternion.AngleAxis(currentCamRot.x, Vector3.right);
+    }
+}
